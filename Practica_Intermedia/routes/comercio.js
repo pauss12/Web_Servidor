1400846edb43445aeb846d6fb0ab000cdbaa2a52
pauss12 <paulamendezsv@gmail.com>
2024-03-13 15:19:10 +0100

    Update comercio.js

--- Practica_Intermedia/routes/comercio.js
+++ Practica_Intermedia/routes/comercio.js
@@ -3,11 +3,20 @@ const express = require("express")
 
 const router = express.Router()
 
-const { getComercios, getComercio, createComercio } = require("../controllers/comercio")
+const { getComercios, getComercio, createComercio, deleteComercio, updateComercio } = require("../controllers/comercio")
 
-
+//RUTAS DE LAS LLAMADAS A GET ----------------
 router.get("/", getComercios)
+
 router.get("/:id", getComercio)
+
+//RUTAS DE LAS LLAMADAS A POST ----------------
 router.post("/", createComercio)
+
+//RUTAS DE LAS LLAMADAS A DELETE ----------------
+router.delete("/:id", deleteComercio)
+
+//RUTAS DE LAS LLAMADAS A PUT ----------------
+router.put("/:id", updateComercio)
 
 module.exports = router
