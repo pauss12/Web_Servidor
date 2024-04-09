462606b0707cea9dd39522b797bc1fed56a147f9
pauss12 <paulamendezsv@gmail.com>
2024-04-09 17:23:13 +0200

    funcion de errores y funcion de handlevalidator

---
+++ practica_final_web/backend/utils/handleValidator.js
@@ -0,0 +1,18 @@
+const { validationResult } = require("express-validator")
+
+const validateResults = (req, res, next) => {
+
+    try {
+
+        validationResult(req).throw()
+        return next()
+
+    } catch (err) {
+        
+        res.status(403)
+        res.send({ errors: err.array() })
+        //console.log(err)
+    }
+}
+
+module.exports = validateResults
