8d52647bbb88cbe9b6ffcfb3ba28c8390a67964a
pauss12 <paulamendezsv@gmail.com>
2024-04-09 17:23:29 +0200

    validadores para el usuario hechos

---
+++ practica_final_web/backend/validators/users.js
@@ -0,0 +1,40 @@
+const { check } = require("express-validator")
+const validateResults = require("../utils/handleValidator")
+
+const validatorGetItem = [
+    check("id").exists().notEmpty().isMongoId(),
+    (req, res, next) => {
+        return validateResults(req, res, next)
+    }
+]
+
+const validatorGetUser = [
+    check("username").exists().notEmpty(),
+    (req, res, next) => {
+        return validateResults(req, res, next)
+    }
+]
+
+const validatorGetEmail = [
+    check("email").exists().notEmpty(),
+    (req, res, next) => {
+        return validateResults(req, res, next)
+    }
+]
+
+const validatorUpdateItem = [
+    check("id").exists().notEmpty().isMongoId(),
+    // Al menos uno de los siguientes campos debe estar presente para la actualización
+    check("nombreDelUsuario").optional(),
+    check("email").optional(),
+    check("password").optional(),
+    check("username").optional(),
+    check("biografia").optional(),
+    check("instrumentos").optional(),
+    check("horarioDeDisponibilidad").optional(),
+    check("fotoDePerfil").optional(),
+    check("permiteNotificaciones").optional(),
+    (req, res, next) => validateResults(req, res, next)
+];
+
+module.exports = { validatorGetItem, validatorGetUser, validatorGetEmail, validatorUpdateItem }
