9ee75e220aaf25cc80546edd7639220620fcee27
pauss12 <paulamendezsv@gmail.com>
2024-04-09 16:59:30 +0200

    programa principal, app.js

---
+++ practica_final_web/backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const dbConnect = require('./config/mongo')
+
+const cors = require("cors")
+
+require('dotenv').config();
+
+const app = express()
+
+//Le decimos a la app de express() que use cors para evitar el error Cross-Domain (XD)
+app.use(cors())
+app.use(express.json())
+
+//app.use("/api", require("./backend/routes"))
+
+const port = process.env.PORT || 3000
+
+dbConnect()
+
+app.listen(port, () => {
+    console.log("Servidor escuchando en el puerto " + port)
+
+})
