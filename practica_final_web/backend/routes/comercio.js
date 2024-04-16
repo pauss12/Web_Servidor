9784cd00b5e8f8d535d999ca14d8e40fb7b5a75b
pauss12 <paulamendezsv@gmail.com>
2024-04-16 10:51:24 +0200

    routes del comercio

--- practica_final_web/backend/routes/comercio.js
+++ practica_final_web/backend/routes/comercio.js
@@ -1,3 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const { getComercios, getComercio, deleteComercio, updateComercio, createComercio } = require('../controllers/comercio');
+
+const { validatorGetComercio, validatorUpdateComercio, validatorCreateItem } = require('../validators/comercio');
+
+router.post('/', createComercio);
+
+router.get('/', getComercios);
+
+router.get('/:id', getComercio);
+
+/*router.post('/', validatorCreateItem, createComercio);*/
+
+
+
+router.put('/:id', updateComercio);
+
+/*router.delete("/:id", validatorGetComercio, deleteComercio);*/
+
+module.exports = router;
