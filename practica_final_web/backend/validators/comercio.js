3cc5fc8e4fd37fa2d40bc0f0463b859a0ce53728
pauss12 <paulamendezsv@gmail.com>
2024-04-17 20:39:59 +0200

    funcion update

--- practica_final_web/backend/validators/comercio.js
+++ practica_final_web/backend/validators/comercio.js
@@ -3,11 +3,12 @@ const { validateResults } = require("../utils/handleValidator")
 
 const validatorGetItem = [
 
-    check("cifComercio").exists().notEmpty(),
+    check("id").exists().notEmpty().isMongoId(),
     (req, res, next) => {
         return validateResults(req, res, next)
     }
 ]
+
 
 const validatorCreateItem = [
 
@@ -30,4 +31,28 @@ const validatorCreateItem = [
 
 ]
 
-module.exports = { validatorGetItem, validatorCreateItem }
+const validatorUpdateComercio = [
+
+    //Checkear el id de mongo
+    check("id").exists().notEmpty().isMongoId(),
+    //Checkear si el resto como opcional
+    check("nombreComercio").optional().notEmpty().isString(),
+    check("cifComercio").optional().notEmpty().isString(),
+    check("direccion").optional().notEmpty().isString(),
+    check("email").optional().notEmpty().isString(),
+    check("telefonoContacto").optional().notEmpty().isString(),
+    check("idPagina").optional().notEmpty(),
+    check("ciudad").optional().notEmpty().isString(),
+    check("actividad").optional().notEmpty().isString(),
+    check("textos").optional().isArray(),
+    check("fotos").optional().isArray(),
+    check("scoring").optional(),
+    check("numeroPuntuaciones").optional(),
+    check("reseñas").optional().isArray(),
+    (req, res, next) => {
+        return validateResults(req, res, next)
+    }
+
+]
+
+module.exports = { validatorGetItem, validatorCreateItem, validatorUpdateComercio }
