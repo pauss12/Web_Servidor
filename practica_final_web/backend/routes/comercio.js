1f669c61631deef781b5893aa79c9bbe07489a6d
pauss12 <paulamendezsv@gmail.com>
2024-04-17 20:40:28 +0200

    Update comercio.js

--- practica_final_web/backend/routes/comercio.js
+++ practica_final_web/backend/routes/comercio.js
@@ -1,22 +1,19 @@
 const express = require('express');
+
 const router = express.Router();
 
 const { getComercios, getComercio, deleteComercio, updateComercio, createComercio } = require('../controllers/comercio');
 
-const { validatorGetComercio, validatorUpdateComercio, validatorCreateItem } = require('../validators/comercio');
+const { validatorGetComercio, validatorCreateItem, validatorUpdateComercio } = require('../validators/comercio');
 
-router.post('/', createComercio);
+router.post('/', validatorCreateItem, createComercio);
 
 router.get('/', getComercios);
 
 router.get('/:id', getComercio);
 
-/*router.post('/', validatorCreateItem, createComercio);*/
+router.put('/:id', validatorUpdateComercio, updateComercio);
 
-
-
-router.put('/:id', updateComercio);
-
-/*router.delete("/:id", validatorGetComercio, deleteComercio);*/
+router.delete('/:id', validatorGetComercio, deleteComercio);
 
 module.exports = router;
