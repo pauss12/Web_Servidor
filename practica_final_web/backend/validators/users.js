ccbc4c0250a6b321af2e020f3b95a842c3c01aa4
pauss12 <paulamendezsv@gmail.com>
2024-04-17 20:39:35 +0200

    Update users.js

--- practica_final_web/backend/validators/users.js
+++ practica_final_web/backend/validators/users.js
@@ -1,5 +1,5 @@
 const { check } = require("express-validator")
-const validateResults = require("../utils/handleValidator")
+const { validateResults } = require("../utils/handleValidator")
 
 const validatorGetItem = [
     check("id").exists().notEmpty().isMongoId(),
