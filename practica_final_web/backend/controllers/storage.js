936f1fe51cf77554420c5338198b40b3fae3e32b
pauss12 <paulamendezsv@gmail.com>
2024-04-26 00:49:20 +0200

    he creado el fichero storage del controllers

---
+++ practica_final_web/backend/controllers/storage.js
@@ -0,0 +1,14 @@
+const router = require("../routes")
+
+const storageModel = require("../models/nosql/storage")
+
+const createItem = async (req, res) => {
+    const { body, file } = req
+    const fileData = {
+        filename: file.filename,
+        url: process.env.PUBLIC_URL + "/" + file.filename
+    }
+    const data = await storageModel.create(fileData)
+    res.send(data)
+}
+module.exports = { createItem }
