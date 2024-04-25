dba09bd5fb137be273d43aa100299be4ed28b557
pauss12 <paulamendezsv@gmail.com>
2024-04-25 23:14:26 +0200

    funciona las pruebas unitarias de register, get users y delete user

---
+++ practica_final_web/backend/test/app.test.js
@@ -0,0 +1,55 @@
+const request = require('supertest');
+const app = require('../app')
+
+describe('users', () => {
+
+    var token = ""
+    var id = ""
+
+    it('should register a user', async () => {
+
+        const response = await request(app)
+            .post('/api/auth/register')
+            .send({
+                "nombreUsuario": "Menganito1",
+                "emailUsuario": "user261@test.com",
+                "passwordUsuario": "HolaMundo.01",
+                "edadUsuario": 20,
+                "ciudadUsuario": "Madrid",
+                "interesesUsuario": ["futbol", "baloncesto"],
+                "permiteOfertas": true
+            })
+            .set('Accept', 'application/json')
+            .expect(200)
+        expect(response.body.user.emailUsuario).toEqual('user261@test.com'),
+
+        token = response.body.token
+        id = response.body.user._id
+    })
+    /*
+        it('should get a Unauthorized error', async () => {
+            const response = await request(app)
+                .get('/api/auth/users')
+                .set('Accept', 'application/json')
+                .expect(401)
+        });
+    */
+    it('should get the users', async () => {
+        const response = await request(app)
+            .get('/api/users')
+            .auth(token, { type: 'bearer' })
+            .set('Accept', 'application/json')
+            .expect(200)
+        expect(response.body.pop().nombreUsuario).toEqual('Menganito1')
+    });
+
+    it('should delete a user', async () => {
+        const response = await request(app)
+            .delete('/api/users/' + id)
+            .auth(token, { type: 'bearer' })
+            .set('Accept', 'application/json')
+            .expect(200)
+        expect(response.body.acknowledged).toEqual(true)
+    });
+
+})
