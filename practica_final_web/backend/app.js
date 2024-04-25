4aeb13dfaf91b3347208f41eae9b867107233868
pauss12 <paulamendezsv@gmail.com>
2024-04-25 23:57:18 +0200

    cosas q necesita para usar el swagger

--- practica_final_web/backend/app.js
+++ practica_final_web/backend/app.js
@@ -1,10 +1,18 @@
+require('dotenv').config();
+
 const express = require("express")
+
+/*
+    const morganBody = require("morgan-body")
+const { IncomingWebhook } = require("@slack/webhook")
+*/ 
+
+const swaggerUi = require("swagger-ui-express")
+const swaggerSpecs = require("./docs/swagger")
 
 const dbConnect = require('./config/mongo')
 
 const cors = require("cors")
-
-require('dotenv').config();
 
 const app = express()
 
@@ -13,7 +21,13 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs))
+
 app.use("/api", require("./routes"))
+
+//app.use(express.static("storage"))//localhost:3000/file.jpg
+
+//const webHook = new IncomingWebhook(process.env.SLACK_WEBHOOK)
 
 const port = process.env.PORT || 3000
 
@@ -23,5 +37,25 @@ app.listen(port, () => {
     console.log("Servidor escuchando en el puerto " + port)
 
 })
+
+/*
+
+const loggerStream = {
+    write: message => {
+        webHook.send({ text: message })
+    },
+}
+
+morganBody(app, {
+    noColors: true,
+
+    //limpiamos el String de datos lo máximo posible antes de mandarlo a Slack
+    skip: function (req, res) {
+        //Solo enviamos errores (4XX de cliente y 5XX de servidor)
+        return res.statusCode < 400
+    }, stream: loggerStream
+})
+
+*/
 
 module.exports = app
