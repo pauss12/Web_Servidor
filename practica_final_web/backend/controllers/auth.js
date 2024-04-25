03ed45990b8086894113358c037931037ea9d19a
pauss12 <paulamendezsv@gmail.com>
2024-04-25 18:57:04 +0200

    Update auth.js

--- practica_final_web/backend/controllers/auth.js
+++ practica_final_web/backend/controllers/auth.js
@@ -13,46 +13,32 @@ const { usersModel } = require("../models")
  * @param {*} req 
  * @param {*} res 
  */
-/*const registerControl = async (req, res) => {
+const registerControl = async (req, res) => {
     
     try {
+
         req = matchedData(req)
+
         const password = await encrypt(req.password)
-        const body = { ...req, password } // Con "..." duplicamos el objeto y le añadimos o sobreescribimos una propiedad
+
+        const body = { ...req, password }
+
         const dataUser = await usersModel.create(body)
-        //Si no queremos que se devuelva el hash con "findOne", en el modelo de users ponemos select: false en el campo password
-        //Además, en este caso con "create", debemos setear la propiedad tal que:  
+        
         dataUser.set('password', undefined, { strict: false })
 
         const data = {
             token: await tokenSign(dataUser),
             user: dataUser
         }
+
         res.send(data)
+
     } catch (err) {
-        console.log(err)
+        //console.log(err)
         handleHttpError(res, "ERROR_REGISTER_USER")
     }
-}*/
-
-const registerControl = async (req, res) => {
-
-    try {
-
-        const body = matchedData(req)
-
-        const data = await usersModel.create(body)
-
-        res.status(201).send(data)
-
-    } catch (err) {
-
-        console.log(err)
-        handleHttpError(res, 'ERROR_CREATE_USUARIO')
-    }
-
 }
-
 
 /**
  * Encargado de hacer login del usuario
