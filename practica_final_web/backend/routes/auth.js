9503eb62928cf1a2d9b6b350ded9f3d54e0d8789
pauss12 <paulamendezsv@gmail.com>
2024-04-25 23:57:49 +0200

    comentario en la ruta del register

--- practica_final_web/backend/routes/auth.js
+++ practica_final_web/backend/routes/auth.js
@@ -3,6 +3,28 @@ const router = express.Router()
 
 const { registerControl, loginControl } = require("../controllers/auth")
 
+/**
+*  @openapi 
+*  /api/auth/register:
+*   post:
+*       tags:
+*       - User
+*       summary: "User registter"
+*       description: Register a new user in the Database
+*       requestBody: 
+*           content: 
+*               application/json: 
+*                   schema:
+*                       $ref: "#/components/schemas/register"          
+*       responses:
+*           '200':
+*               description: Returns the inserted object
+*           '401':
+*               description: Validation error 
+* 
+*       security:
+*         - bearerAuth: []
+*/
 const { validatorRegister, validatorLogin } = require("../validators/auth")
 
 router.post("/register", validatorRegister, registerControl)
