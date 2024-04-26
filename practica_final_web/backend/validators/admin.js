442324cc8560b93454790ae070fb035b22a17d2e
pauss12 <paulamendezsv@gmail.com>
2024-04-26 13:47:09 +0200

    validator del create comercio

---
+++ practica_final_web/backend/validators/admin.js
@@ -0,0 +1,18 @@
+const { check } = require("express-validator")
+
+const { validateResults } = require("../utils/handleValidator")
+
+
+
+const validatorCreateComercio = [
+
+    check("nombreComercio").exists().notEmpty(),
+    check("direccionComercio").exists().notEmpty(),
+    check("telefonoComercio").exists().notEmpty(),
+    check("emailComercio").exists().notEmpty(),
+    (req, res, next) => {
+        return validateResults(req, res, next)
+    }
+];
+
+module.exports = { validatorCreateComercio }
