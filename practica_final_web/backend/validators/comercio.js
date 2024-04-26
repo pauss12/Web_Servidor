2470b2f343f562e6cb440a464710f6f273d78b68
pauss12 <paulamendezsv@gmail.com>
2024-04-26 16:47:27 +0200

    quitar un validador de update comercio de comercio.js

--- practica_final_web/backend/validators/comercio.js
+++ practica_final_web/backend/validators/comercio.js
@@ -41,19 +41,4 @@ const validatorUpdateItemAdmin = [
 
 ]
 
-const validatorUpdateItemComercio = [
-
-    //Checkear el id de mongo
-    check("id").exists().notEmpty().isMongoId(),
-    
-    check("idPagina").optional().notEmpty().isNumeric(),
-    check("ciudad").optional().notEmpty(),
-    check("actividad").optional().notEmpty(),
-    check("textos").optional().isArray(),
-    check("fotos").optional().isArray(),
-    (req, res, next) => {
-        return validateResults(req, res, next)
-    }
-]
-
-module.exports = { validatorGetItem, validatorCreateItem, validatorUpdateItemAdmin, validatorUpdateItemComercio }
+module.exports = { validatorGetItem, validatorCreateItem, validatorUpdateItemAdmin }
