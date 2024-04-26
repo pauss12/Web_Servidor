3b7e460a862678b50209fc6856c7d05ae6de2f94
pauss12 <paulamendezsv@gmail.com>
2024-04-26 16:46:31 +0200

    inicio del login de comercio

--- practica_final_web/backend/controllers/paginaComercio.js
+++ practica_final_web/backend/controllers/paginaComercio.js
@@ -11,7 +11,6 @@ const { encrypt, compare } = require("../utils/handlePassword")
 /**
  * Obtener lista de paginas de comercio de la base de datos sin ningun tipo de fitro
 */
-
 const getPaginasComercio = async (req, res) => {
 
     try {
@@ -26,6 +25,65 @@ const getPaginasComercio = async (req, res) => {
     }
 }
 
+const loginComercio = async (req, res) => {
+
+    try {
+
+        req = matchedData(req)
+
+        /*const comercio = await paginaModel.findOne({ _id: req._id }).select("passwordComercio nombreUsuario role emailUsuario")*/
+
+        const comercio = await paginaModel.findOne({ emailComercio: req.emailComercio }).select("passwordComercio nombreComercio _id emailComercio")
+
+        console.log(comercio)
+
+        if (!comercio) {
+            handleHttpError(res, "COMERCIO_NOT_EXISTS", 404)
+            return
+        }
+
+        const hashPassword = comercio.passwordComercio;
+
+        const check = await compare(req.passwordComercio, hashPassword)
+
+        if (!check) {
+            handleHttpError(res, "INVALID_PASSWORD", 401)
+            return
+        }
+
+        //Si no quisiera devolver el hash del password
+        comercio.set('passwordComercio', undefined, { strict: false })
+
+        const data = {
+            token: await tokenSign(comercio),
+            comercio
+        }
+
+        res.send(data)
+
+    } catch (err) {
+        console.log(err)
+        handleHttpError(res, "ERROR_LOGIN_USER")
+    }
+}
+
+const createPaginaComercio = async (req, res) => {
+
+    try {
+
+        const body = matchedData(req)
+
+        const data = await paginaModel.create(body)
+        res.status(201).send(data)
+
+    } catch (err) {
+
+        console.log(err)
+
+        handleHttpError(res, "ERROR_CREATE_PAGINA_COMERCIO")
+    }
+}
 
 
-module.exports = { getPaginasComercio }
+
+module.exports = { getPaginasComercio, createPaginaComercio, loginComercio }
