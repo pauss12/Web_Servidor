b2c169e148d741efedd9183b8a5a4dda32868766
pauss12 <paulamendezsv@gmail.com>
2024-04-26 16:20:48 +0200

    funcion checkear comercio, que checkea el id del comercio del token, con el  que esta en la BBDD

--- practica_final_web/backend/middleware/rol.js
+++ practica_final_web/backend/middleware/rol.js
@@ -1,8 +1,11 @@
 const { handleHttpError } = require("../utils/handleError")
 
+const JWT_SECRET = process.env.JWT_SECRET
+
 const checkRol = (roles) => (req, res, next) => {
 
     try {
+
         const { user } = req
 
         const userRol = user.role
@@ -12,7 +15,7 @@ const checkRol = (roles) => (req, res, next) => {
         if (!checkValueRol) {
 
             handleHttpError(res, "NOT_ALLOWED", 403)
-            return
+            return ;
         }
 
         next()
@@ -23,4 +26,35 @@ const checkRol = (roles) => (req, res, next) => {
     }
 }
 
-module.exports = checkRol
+//Checkear que el comercio que se va a editar sea del comercio propio
+const checkearComercio = (req, res, next) => {
+
+    console.log("checkearComercio")
+    
+    try {
+        
+        // Verificar el token
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        console.log('decoded:', decoded);
+
+
+        // Obtener el email del comercio de la base de datos
+        const emailBBDD = req.user.email;
+
+        console.log('emailBBDD:', emailBBDD);
+
+        // Comparar el email del token con el email de la base de datos
+        if (decoded.email === emailBBDD)
+            return true; 
+        else
+            return false; 
+
+    } catch (error) {
+        // Manejar errores de verificación del token
+        console.error('Error al verificar el token:', error);
+        return false;
+    }
+}
+
+module.exports = { checkRol, checkearComercio }
