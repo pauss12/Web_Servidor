78e57f808bdb4035c0283cfa21968f0e7133757e
pauss12 <paulamendezsv@gmail.com>
2024-04-26 16:23:08 +0200

    validadores de crear pagina comercio y actualizar la pagina, hechos!

---
+++ practica_final_web/backend/validators/paginaComercio.js
@@ -0,0 +1,33 @@
+const { check } = require("express-validator")
+const { validateResults } = require("../utils/handleValidator")
+
+const validatorCrearPaginaComercio = [
+
+    check("id").exists().notEmpty().isMongoId(),
+    check("ciudad").exists().notEmpty().isString(),
+    check("actividad").exists().notEmpty().isString(),
+    check("titulo").exists().notEmpty().isString(),
+    check("resumen").exists().notEmpty().isString(),
+    check("textos").exists().notEmpty().isArray(),
+    check("fotos").exists().notEmpty().isArray(),
+    (req, res, next) => {
+        return validateResults(req, res, next)
+    }
+]
+
+const validatorUpdateItemComercio = [
+
+    //Checkear el id de mongo
+    check("id").exists().notEmpty().isMongoId(),
+
+    check("idPagina").optional().notEmpty().isNumeric(),
+    check("ciudad").optional().notEmpty(),
+    check("actividad").optional().notEmpty(),
+    check("textos").optional().isArray(),
+    check("fotos").optional().isArray(),
+    (req, res, next) => {
+        return validateResults(req, res, next)
+    }
+]
+
+module.exports = { validatorCrearPaginaComercio, validatorUpdateItemComercio }
