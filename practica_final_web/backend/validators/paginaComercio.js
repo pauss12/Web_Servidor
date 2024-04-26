cdbe2cff5bceeef8f32905b97ba27388ada82683
pauss12 <paulamendezsv@gmail.com>
2024-04-26 16:47:31 +0200

    Update paginaComercio.js

--- practica_final_web/backend/validators/paginaComercio.js
+++ practica_final_web/backend/validators/paginaComercio.js
@@ -3,11 +3,12 @@ const { validateResults } = require("../utils/handleValidator")
 
 const validatorCrearPaginaComercio = [
 
-    check("id").exists().notEmpty().isMongoId(),
-    check("ciudad").exists().notEmpty().isString(),
-    check("actividad").exists().notEmpty().isString(),
-    check("titulo").exists().notEmpty().isString(),
-    check("resumen").exists().notEmpty().isString(),
+    
+    check("idPagina").optional().notEmpty().isNumeric(),
+    check("titulo").optional().notEmpty().isString(),
+    check("ciudadComercio").optional().notEmpty(),
+    check("actividadComercio").optional().notEmpty(),
+    check("resumenComercio").optional().notEmpty().
     check("textos").optional().isArray(),
     check("fotos").optional().isArray(),
     (req, res, next) => {
@@ -21,8 +22,10 @@ const validatorUpdateItemComercio = [
     check("id").exists().notEmpty().isMongoId(),
 
     check("idPagina").optional().notEmpty().isNumeric(),
-    check("ciudad").optional().notEmpty(),
-    check("actividad").optional().notEmpty(),
+    check("titulo").optional().notEmpty().isString(),
+    check("ciudadComercio").optional().notEmpty(),
+    check("actividadComercio").optional().notEmpty(),
+    check("resumenComercio").optional().notEmpty().isString(),
     check("textos").optional().isArray(),
     check("fotos").optional().isArray(),
     (req, res, next) => {
