c32549037ce78a5747fdc8ba88b51510fb441ae3
pauss12 <paulamendezsv@gmail.com>
2024-04-27 15:09:04 +0200

    he puesto algunos comentarios de swagger

--- practica_final_web/backend/routes/comercio.js
+++ practica_final_web/backend/routes/comercio.js
@@ -7,25 +7,70 @@ const { getComercios, getComercio, deleteComercio, updateComercio, createComerci
 
 const { validatorGetItem, validatorUpdateItemAdmin, validatorCreateItem, validatorLoginComercio } = require('../validators/comercio');
 
-//Obtener lista de comercios
+/**
+ * @openapi
+ * /api/comercio:
+ *  get:
+ *      tags:
+ *      - Comercio
+ *      summary: Get merchants from the database
+ *      description: ''
+ *      responses:
+ *          '200':
+ *              description: Returns the merchants
+ *          '403':
+ *              description: Error fetching merchants
+ */
 router.get('/', getComercios);
 
-//Obtener un comercio en especifico por id
+/**
+*   @openapi
+*  /api/comercio/{id}:
+*  get:
+*      tags:
+*      - Comercio
+*      summary: Get a merchant by id
+*      description: Get a merchant by id
+*      parameters:
+*          -   name: id
+*              in: path
+*              required: true
+*              schema:
+*                  type: string
+*      responses:
+*          '200':
+*              description: Returns the merchant
+*          '403':
+*              description: Error fetching Merchant
+*/
 router.get('/:id', validatorGetItem, getComercio);
 
 //Login comercio
 router.post('/loginComercio', checkearComercio, validatorLoginComercio, loginComercio);
 
-//crear comercio
-router.post('/', authMiddleware, checkRol(["admin"]), validatorCreateItem, createComercio);
 
-//Modificar comercio siendo admin
-router.put('/:id', authMiddleware, checkRol(["admin"]), validatorUpdateItemAdmin, updateComercio);
 
-//Borrar comercio siendo admin
-router.delete('/:id', authMiddleware, checkRol(["admin"]), validatorGetItem, deleteComercio);
-
-//Borrar comercio siendo el dueño del comercio
+/**
+ * @openapi
+ * /api/comercio/{id}:
+ *  delete:
+ *      tags:
+ *      - Comercio
+ *      summary: Delete merchant himself
+ *      description: Delete a merchant himself; checks the token we have in the header and after that, checks if the ID from the token is the same as the ID from the URL, if it is, it will allow us to delete the merchant
+ *      parameters:
+ *          -   name: id
+ *              in: path
+ *              description: id that need to be deleted
+ *              required: true
+ *      responses:
+ *          '200':
+ *              description: Returns the inserted object
+ *          '401':
+ *              description: Validation error
+ *      security:
+ *          - bearerAuth: []
+ */
 router.delete('/:id', checkearComercio, validatorGetItem, deleteComercio);
 
 module.exports = router;
