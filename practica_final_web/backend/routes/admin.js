46cd425c9391764fd345a826e6a29576023e5dae
pauss12 <paulamendezsv@gmail.com>
2024-04-27 16:55:19 +0200

    routes admin.js

---
+++ practica_final_web/backend/routes/admin.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const { checkRol } = require('../middleware/rol');
+
+const { authMiddleware } = require("../middleware/session")
+
+const router = express.Router();
+
+const { createComercio } = require('../controllers/admin');
+
+const { validatorCreateItem, validatorUpdateItemAdmin, validatorGetItem } = require('../validators/comercio');
+
+
+//crear comercio
+router.post('/', authMiddleware, checkRol(["admin"]), validatorCreateItem, createComercio);
+
+//Modificar comercio siendo admin
+router.put('/:id', authMiddleware, checkRol(["admin"]), validatorUpdateItemAdmin, updateComercio);
+
+/**
+ * @openapi
+ * /api/admin/{id}:
+ *  delete:
+ *      tags:
+ *      - Comercio
+ *      summary: Delete merchant by admin
+ *      description: Delete a merchant by an admin; checks the token we have in the header and after that, checks the role of the users whose token we have in the header and if it is admin, it will allow us to delete the merchant
+ *      parameters:
+ *          -   name: id
+ *              in: path
+ *              description: id that need to be deleted
+ *              required: true
+ *      responses:
+ *          '200':
+ *              description: Returns the inserted object
+ *          '401':
+ *              description: Validation error
+ *      security:
+ *          - bearerAuth: []
+ */
+router.delete('/:id', authMiddleware, checkRol(["admin"]), validatorGetItem, deleteComercio);
+
+
+
+module.exports = router;
