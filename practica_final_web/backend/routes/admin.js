0e1b829b000c54bf3d7f2980fd8e79d1f85d54a6
pauss12 <paulamendezsv@gmail.com>
2024-04-29 17:07:06 +0200

    update y crear comercio; comentarios para el swagger

--- practica_final_web/backend/routes/admin.js
+++ practica_final_web/backend/routes/admin.js
@@ -13,10 +13,57 @@ const { updateComercio, deleteComercio } = require('../controllers/comercio');
 const { validatorCreateItem, validatorUpdateItemAdmin, validatorGetItem } = require('../validators/comercio');
 
 
-//crear comercio
+/** 
+ * @openapi
+ * /api/api/admin/crearComercio:
+ *  post:
+ *      tags:
+ *      - Admin
+ *      summary: Create Merchant by admin
+ *      description: Create a Merchant by an admin; checks the token we have in the header and after that, checks the role of the users whose token we have in the header and if it is admin, it will allow us to create the merchant
+ *      requestBody:
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/comercio"
+ *      responses:
+ *          '200':
+ *              description: Returns the inserted object and the token of the merchant created
+ *          '403':
+ *              description: Validation error
+ *      security:
+ *          - bearerAuth: []
+ */
 router.post('/', authMiddleware, checkRol(["admin"]), validatorCreateItem, createComercio);
 
-//Modificar comercio siendo admin
+/** 
+ * @openapi
+ * /api/auth/update/{id}:
+ *  put:
+ *      tags:
+ *      - Admin
+ *      summary: Update Merchant by admin
+ *      description: Update a Merchant by an admin; checks the token we have in the header and after that, checks the role of the users whose token we have in the header and if it is admin, it will allow us to update the merchant
+ *      parameters:
+ *          -   name: id
+ *              in: path
+ *              description: id that need to be updated
+ *              required: true
+ *              schema:
+ *                  type: string
+ *      requestBody:
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/comercio"
+ *      responses:
+ *          '200':
+ *              description: Returns the inserted object
+ *          '403':
+ *              description: Validation error
+ *      security:
+ *          - bearerAuth: []
+ */
 router.put('/:id', authMiddleware, checkRol(["admin"]), validatorUpdateItemAdmin, updateComercio);
 
 /**
