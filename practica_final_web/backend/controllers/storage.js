aed41c8d945140bf89ace9f4d80392df4a27eac7
pauss12 <paulamendezsv@gmail.com>
2024-04-30 19:24:52 +0200

    intento de subir una foto; ns muy bien que deberia de haber en este fichero

--- practica_final_web/backend/controllers/storage.js
+++ practica_final_web/backend/controllers/storage.js
@@ -3,12 +3,19 @@ const router = require("../routes")
 const storageModel = require("../models/nosql/storage")
 
 const createItem = async (req, res) => {
+
     const { body, file } = req
+
+    console.log(body)
+
     const fileData = {
         filename: file.filename,
-        url: process.env.PUBLIC_URL + "/" + file.filename
+        idPropietario: body.idPropietario
     }
+
     const data = await storageModel.create(fileData)
+
     res.send(data)
 }
+
 module.exports = { createItem }
