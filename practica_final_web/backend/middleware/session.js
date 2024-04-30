e751516ebda9edf2bd83b3d9737c91f0bbaed584
pauss12 <paulamendezsv@gmail.com>
2024-04-30 13:25:01 +0200

    he quitado comentarios

--- practica_final_web/backend/middleware/session.js
+++ practica_final_web/backend/middleware/session.js
@@ -11,10 +11,8 @@ const authMiddleware = async (req, res, next) => {
             return
         }
 
-        // Nos llega la palabra reservada Bearer (es un estándar) y el Token, así que me quedo con la última parte
         const token = req.headers.authorization.split(' ').pop()
 
-        //Del token, miramos en Payload (revisar verifyToken de utils/handleJwt)
         const dataToken = await verifyToken(token)
 
         if (!dataToken) {
@@ -29,10 +27,8 @@ const authMiddleware = async (req, res, next) => {
         
         const user = await usersModel.findById(dataToken._id)
 
-        //Añadimos el usuario a la request
         req.user = user
 
-        //Pasamos al siguiente middleware
         next()
 
     } catch (err) {
