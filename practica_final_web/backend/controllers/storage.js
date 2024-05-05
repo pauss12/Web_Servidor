d54102916089b6433ee4ea56e70e6888e5a3d0e5
pauss12 <paulamendezsv@gmail.com>
2024-05-05 19:10:40 +0200

    subida la imagen

--- practica_final_web/backend/controllers/storage.js
+++ practica_final_web/backend/controllers/storage.js
@@ -1,19 +1,33 @@
 const router = require("../routes")
 
-const storageModel = require("../models/nosql/storage")
+const { storageModel, paginaModel} = require("../models")
 
 const createItem = async (req, res) => {
 
     const { body, file } = req
 
-    console.log(body)
+    const { id } = req.params
+
+    console.log(id)
 
     const fileData = {
+
         filename: file.filename,
-        idPropietario: body.idPropietario
+        url: process.env.PUBLIC_URL + "/" + file.filename,
+        idPropietario: id
     }
 
     const data = await storageModel.create(fileData)
+
+    //Tengo que añadir la ruta de la imagen en la variable de fotos de la pagina del comercio al que pertenece
+    
+    const paginaComercio = await paginaModel.findOne({ _id: id })
+
+    const fotos = paginaComercio.fotos;
+
+    fotos.push(fileData.url)
+
+    await paginaModel.updateOne({ _id: id }, { fotos: fotos })
 
     res.send(data)
 }
