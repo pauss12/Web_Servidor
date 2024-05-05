1487a007f8b6d8b5a80cced9bbb305b6eacad8b3
pauss12 <paulamendezsv@gmail.com>
2024-05-06 00:21:09 +0200

    he cambiado la ruta para subir una foto

--- practica_final_web/backend/routes/storage.js
+++ practica_final_web/backend/routes/storage.js
@@ -6,6 +6,6 @@ const uploadMiddleware = require("../utils/handleStorage")
 
 const { createItem } = require("../controllers/storage")
 
-router.post("/:id", uploadMiddleware.single("image"), createItem)
+router.post("/photos/:id", uploadMiddleware.single("image"), createItem)
 
 module.exports = router
