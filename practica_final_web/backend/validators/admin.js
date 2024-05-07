7c6bdd0c00dc06ad2dc1dc39015f3b330e72dd4d
pauss12 <paulamendezsv@gmail.com>
2024-05-07 18:29:12 +0200

    actualizacion validator crear comercio

--- practica_final_web/backend/validators/admin.js
+++ practica_final_web/backend/validators/admin.js
@@ -6,9 +6,11 @@ const { validateResults } = require("../utils/handleValidator")
 const validatorCreateComercio = [
 
     check("nombreComercio").exists().notEmpty(),
+    check("cifComercio").exists().notEmpty(),
     check("direccionComercio").exists().notEmpty(),
-    check("telefonoComercio").exists().notEmpty(),
     check("emailComercio").exists().notEmpty(),
+    check("passwordComercio").exists().notEmpty(),
+    check("telefonoComercio").exists().notEmpty(),
     (req, res, next) => {
         return validateResults(req, res, next)
     }
