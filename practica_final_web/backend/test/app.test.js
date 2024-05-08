48c131eb1b9180891b2e0f3bf44e5df7c5f87a1f
pauss12 <paulamendezsv@gmail.com>
2024-05-08 22:12:46 +0200

    me funcionan los test menos el crear comercio; los probados al menos

--- practica_final_web/backend/test/app.test.js
+++ practica_final_web/backend/test/app.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest');
 const app = require('../app')
 
-describe('users', () => {
+describe('\nUSUARIOS', () => {
 
     var token = ""
     var id = ""
+    var city = ""
 
     it('should register a user', async () => {
 
@@ -34,27 +35,86 @@ describe('users', () => {
             .set('Accept', 'application/json')
             .expect(401)
     });*/
-    
-    it('should get the users', async () => {
+
+    it('should login a user', async () => {
+
         const response = await request(app)
-            .get('/api/users')
-            .auth(token, { type: 'bearer' })
+            .post('/api/auth/login')
+            .send({
+
+                "emailUsuario": "user261@test.com",
+                "passwordUsuario": "HolaMundo.01"
+            })
             .set('Accept', 'application/json')
             .expect(200)
-        expect(response.body.pop().nombreUsuario).toEqual('Menganito1')
-    });
+
+        token = response.body.token
+        id = response.body.user._id
+
+        console.log("TOKEN USUARIO desde el login de usuario " + token)
+
+    })
+
+
+    it('should get the users', async () => {
+
+        const response = await request(app)
+            .get('/api/users')
+            .set('Accept', 'application/json')
+            .expect(200)
+
+    })
 
     it('should get a user', async () => {
         
         const response = await request(app)
             .get('/api/users/' + id)
-            .auth(token, { type: 'bearer' })
             .set('Accept', 'application/json')
             .expect(200)
         
         expect(response.body.nombreUsuario).toEqual('Menganito1')
+        city = response.body.ciudadUsuario
         
-    });
+    })
+
+    it('should get a user from a city', async () => {
+    
+        const response = await request(app)
+            .get('/api/users/user/' + city)
+            .set('Accept', 'application/json')
+            .expect(200)
+        
+        console.log("CIUDAD USUARIO de la respuesta " + response.body.ciudadUsuario)
+        //expect(response.body.ciudadUsuario).toEqual('Madrid')
+
+    })
+
+    it('should update a user', async () => {
+    
+        console.log("ID USUARIO " + id)
+
+        console.log("TOKEN USUARIO " + token)
+
+        const response = await request(app)
+            .put('/api/users/' + id)
+            .send({
+                "nombreUsuario": "Menganito2",
+                "emailUsuario": "user26@test.com",
+                "passwordUsuario": "HolaMundo.01",
+                "edadUsuario": 20,
+                "sexoUsuario": "Hombre",
+                "ciudadUsuario": "Madrid",
+                "interesesUsuario": ["futbol", "baloncesto"],
+                "permiteOfertas": true
+            })
+            .auth(token, { type: 'bearer' })
+            .set('Accept', 'application/json')
+            .expect(200)
+        
+        console.log("NOMBRE USUARIO " + response.body.nombreUsuario)
+        
+        expect(response.token).toEqual(token)
+    })
 
     it('should delete a user', async () => {
         const response = await request(app)
@@ -63,8 +123,76 @@ describe('users', () => {
             .set('Accept', 'application/json')
             .expect(200)
         expect(response.body.acknowledged).toEqual(true)
-    });
+    })
 
+})
+
+describe('\nCOMERCIOS', () => {
+
+    var token = ""
+    var id = ""
+
+    it('should login a admin user', async () => {
+
+        const response = await request(app)
+         .post('/api/auth/login')
+            .send({
+
+                "emailUsuario": "usuario3@gmail.com",
+                "passwordUsuario": "1234578P"
+            })
+            .set('Accept', 'application/json')
+            .expect(200)
     
+        token = response.body.token
 
+        //console.log("TOKEN USUARIO ADMIN desde el login de usuario " + token)
+    })
+    
+    it('should create a merchant', async () => {
+
+        //console.log("TOKEN USUARIO ADMIN " + token)
+
+        const response = await request(app)
+            .post('/api/admin/crearComercio')
+            .send({
+
+                "nombreComercio": "Media Markt",
+                "cifComercio": "12345678",
+                "direccionComercio": "Calle 1",
+                "emailComercio": "mediamarkt@gmail.com",
+                "passwordComercio": "12345678",
+                "telefonoContacto": "4123456789FG"
+            })
+            .auth(token, { type: 'bearer' })
+            .set('Accept', 'application/json')
+            .expect(200)
+        
+        token = response.body.token
+        //id = response.body.dataComercio._id
+
+        id = "663bb3639d4a460b80e5ebf2"
+
+        //console.log("TOKEN COMERCIO " + token)
+        //console.log("ID COMERCIO " + id)
+
+    })
+
+    it('should get the merchants', async () => {
+        const response = await request(app)
+            .get('/api/comercio')
+            .set('Accept', 'application/json')
+            .expect(200)
+        
+    })
+
+    it('should get a merchant', async () => {
+
+        const response = await request(app)
+            .get('/api/comercio/' + id)
+            .set('Accept', 'application/json')
+            .expect(200)
+        
+        //expect(response.body.dataComercio.nombreComercio).toEqual('Comercio 1')
+    })
 })
