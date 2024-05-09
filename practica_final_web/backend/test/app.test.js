566e4c89b703512cc7122065ef84c5beb5dcba12
pauss12 <paulamendezsv@gmail.com>
2024-05-10 01:49:09 +0200

    22 pruebas funcionan ya

--- practica_final_web/backend/test/app.test.js
+++ practica_final_web/backend/test/app.test.js
@@ -12,7 +12,7 @@ describe('\nUSUARIOS', () => {
         const response = await request(app)
             .post('/api/auth/register')
             .send({
-                "nombreUsuario": "Menganito1",
+                "nombreUsuario": "Paula",
                 "emailUsuario": "user261@test.com",
                 "passwordUsuario": "HolaMundo.01",
                 "edadUsuario": 20,
@@ -28,7 +28,7 @@ describe('\nUSUARIOS', () => {
         token = response.body.token
         id = response.body.user._id
     })
-    
+
     /*it('should get a Unauthorized error', async () => {
         const response = await request(app)
             .get('/api/auth/users')
@@ -51,8 +51,6 @@ describe('\nUSUARIOS', () => {
         token = response.body.token
         id = response.body.user._id
 
-        //console.log("TOKEN USUARIO desde el login de usuario " + token)
-
     })
 
 
@@ -66,32 +64,30 @@ describe('\nUSUARIOS', () => {
     })
 
     it('should get a user', async () => {
-        
+
         const response = await request(app)
             .get('/api/users/' + id)
             .set('Accept', 'application/json')
             .expect(200)
-        
-        expect(response.body.nombreUsuario).toEqual('Menganito1')
+
+        expect(response.body.nombreUsuario).toEqual('Paula')
+
         city = response.body.ciudadUsuario
-        
+
     })
 
     it('should get a user from a city', async () => {
-    
+
         const response = await request(app)
             .get('/api/users/user/' + city)
             .set('Accept', 'application/json')
             .expect(200)
         
-        //console.log("CIUDAD USUARIO de la respuesta " + response.body.ciudadUsuario)
-        //expect(response.body.ciudadUsuario).toEqual('Madrid')
+        expect(response.body[0].ciudadUsuario).toEqual(city)
 
     })
 
     it('should update a user', async () => {
-    
-        //console.log("TOKEN USUARIO " + token)
 
         const response = await request(app)
             .put('/api/users/' + id)
@@ -108,17 +104,11 @@ describe('\nUSUARIOS', () => {
             .auth(token, { type: 'bearer' })
             .set('Accept', 'application/json')
             .expect(200)
-        
-        //console.log("NOMBRE USUARIO " + response.body.nombreUsuario)
-        expect(response.body.user.nombreUsuario).toEqual('Menganito2')
 
-        //console.log("TOKEN USUARIO antes de modificarlo ---------" + token)
-        //console.log("TOKEN USUARIO antes de modificarlo RESPUESTA " + token)
+        expect(response.body.user.nombreUsuario).toEqual('Menganito2')
 
         token = response.body.token
         id = response.body.user._id
-
-        //console.log("TOKEN USUARIO al modificarlo" + token)
     })
 
     it('should delete a user', async () => {
@@ -136,121 +126,244 @@ describe('\nCOMERCIOS', () => {
 
     var tokenComercio = ""
     var tokenAdmin = ""
-    var id = ""
+    var idComercio = ""
+    var idWeb = ""
+    var city = ""
 
     it('should login a admin user', async () => {
 
         const response = await request(app)
-         .post('/api/auth/login')
+            .post('/api/auth/login')
+            .set('Accept', 'application/json')
             .send({
 
-                "emailUsuario": "usuario1@gmail.com",
-                "passwordUsuario": "12345678"
+                emailUsuario: "usuario1@gmail.com",
+                passwordUsuario: "12345678"
             })
-            .set('Accept', 'application/json')
             .expect(200)
-    
-        tokenAdmin = ""
+        expect(response.body.user.emailUsuario).toEqual('usuario1@gmail.com')
+        expect(response.body.user.role).toEqual('admin')
+
         tokenAdmin = response.body.token
-
-        //console.log("TOKEN USUARIO ADMIN desde el login de usuario " + token)
     })
-    
-    it('should create a merchant', async () => {
 
-        //console.log("TOKEN USUARIO ADMIN " + tokenAdmin)
+    it('should create a merchant', async () => {
 
         const response = await request(app)
             .post('/api/admin/crearComercio')
+            .set('Accept', 'application/json')
+            .auth(tokenAdmin, { type: 'bearer' })
             .send({
-
                 "nombreComercio": "Media Markt",
                 "cifComercio": "12345678",
                 "direccionComercio": "Calle 1",
                 "emailComercio": "mediamarkt@gmail.com",
                 "passwordComercio": "12345678",
-                "telefonoContacto": "4123456789FG"
+                "telefonoComercio": "611452480",
             })
-            .auth(tokenAdmin, { type: 'bearer' })
-            .set('Accept', 'application/json')
             .expect(200)
-        
-        tokenComercio = response.body.token
-        //id = response.body.dataComercio._id
 
-        //console.log("TOKEN COMERCIO " + token)
-        console.log("ID COMERCIO " + id)
+        tokenComercio = response.body.token
+        idComercio = response.body.dataComercio._id
 
     })
 
     it('should login a merchant', async () => {
 
-        tokenComercio = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjNjMGZjNDc2YjRlMWQ4YmQzZGQ2Y2EiLCJpYXQiOjE3MTUyMTIyMjgsImV4cCI6MTcxNTIxOTQyOH0.fFryOl1q786sFdKyFJ89WbmM52rIo7duEGUoeuiPInM"
-
-        
         const response = await request(app)
             .post('/api/comercio/loginComercio')
             .send({
 
-                "emailComercio": "comercio1@gmail.com",
-                "passwordComercio": "hola12345"
+                "emailComercio": "mediamarkt@gmail.com",
+                "passwordComercio": "12345678"
             })
             .auth(tokenComercio, { type: 'bearer' })
             .set('Accept', 'application/json')
             .expect(200)
 
         tokenComercio = response.body.token
-        id = response.body.dataComercio._id
-
-        //console.log("TOKEN USUARIO desde el login de usuario " + token)
+        idComercio = response.body.dataComercio._id
 
     })
 
 
     it('should get the merchants', async () => {
+
         const response = await request(app)
             .get('/api/comercio')
             .set('Accept', 'application/json')
             .expect(200)
-        
     })
 
     it('should get a merchant', async () => {
 
-        id = "663c0fc476b4e1d8bd3dd6ca"
-
         const response = await request(app)
-            .get('/api/comercio/' + id)
+            .get('/api/comercio/' + idComercio)
             .set('Accept', 'application/json')
             .expect(200)
-        
-        //expect(response.body.dataComercio.nombreComercio).toEqual('Comercio 1')
+
+        expect(response.body.nombreComercio).toEqual('Media Markt')
     })
 
     it('should update a merchant', async () => {
 
-        /*tokenAdmin = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjNiYjc5ZDUyZjYwNmQ3YThmMWE3ZmYiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MTUyMTE2NzUsImV4cCI6MTcxNTIxODg3NX0.H-Bw2Tj283yrmb05dIniTh9mA7mn6zyXFdZvic1nYZ0"*/
-
-        id = "663c0fc476b4e1d8bd3dd6ca"
-
         const response = await request(app)
-            .put('/api/admin/' + id)
+            .put('/api/admin/' + idComercio)
+            .set('Accept', 'application/json')
+            .auth(tokenAdmin, { type: 'bearer' })
             .send({
                 "nombreComercio": "COM",
                 "cifComercio": "12345678",
                 "direccionComercio": "Calle 1",
                 "emailComercio": "com@gmail.com",
                 "passwordComercio": "hola12345",
-                "telefonoContacto": "611452480"
+                "telefonoComercio": "611452480"
             })
-            .auth(tokenAdmin, { type: 'bearer' })
+            .expect(200)
+
+        expect(response.body.dataComercio.nombreComercio).toEqual('COM')
+        
+        tokenComercio = response.body.token
+        idComercio = response.body.dataComercio._id
+
+    })
+
+    it('should create a web page for the merchant', async () => {
+    
+        const response = await request(app)
+            .post('/api/paginaComercio/createPage')
+            .set('Accept', 'application/json')
+            .auth(tokenComercio, { type: 'bearer' })
+            .send({
+                "titulo": "mediaMarkt Web",
+                "ciudadComercio": "Madrid",
+                "actividadComercio": "Electronica",
+                "resumenComercio": "Venta de productos electronicos"
+            })
+            .expect(200)
+
+        idWeb = response.body._id
+        idComercio = response.body.idPagina
+    })
+
+    it('should create a web page for the merchant', async () => {
+
+        const response = await request(app)
+            .post('/api/paginaComercio/createPage')
+            .set('Accept', 'application/json')
+            .auth(tokenComercio, { type: 'bearer' })
+            .send({
+                "titulo": "mediaMarkt Web",
+                "ciudadComercio": "Barcelona",
+                "actividadComercio": "Electronica",
+                "resumenComercio": "Venta de productos electronicos"
+            })
+            .expect(200)
+        
+        city = response.body.ciudadComercio
+
+    })
+
+    it('should get the merchants webpages', async () => {
+
+        const response = await request(app)
+            .get('/api/paginaComercio')
+            .set('Accept', 'application/json')
+            .expect(200)
+
+    })
+
+    it('should get a merchant', async () => {
+
+        const response = await request(app)
+            .get('/api/paginaComercio/' + idWeb)
+            .set('Accept', 'application/json')
+            .expect(200)
+
+        expect(response.body._id).toEqual(idWeb)
+
+    })
+
+    it('should get a merchant from the city ', async () => {
+
+        const response = await request(app)
+            .get('/api/paginaComercio/search/' + city)
             .set('Accept', 'application/json')
             .expect(200)
         
-        tokenComercio = response.body.token
-        id = response.body.dataComercio._id
-        
-        //expect(response.body.dataComercio.nombreComercio).toEqual('Media Markt')
+        expect(response.body[0].ciudadComercio).toEqual(city)
 
+    })
+
+    it('should update a merchant webpage', async () => {
+
+        const response = await request(app)
+            .put('/api/paginaComercio/' + idWeb)
+            .set('Accept', 'application/json')
+            .auth(tokenComercio, { type: 'bearer' })
+            .send({
+                "titulo": "mediaMarkt Web",
+                "ciudadComercio": "Madrid",
+                "actividadComercio": "Electronica",
+                "resumenComercio": "Venta de productos electronicos"
+            })
+            .expect(200)
+
+        expect(response.body.pagina._id).toEqual(idWeb)
+
+        idWeb = response.body.pagina._id
+        idComercio = response.body.pagina.idPagina
+
+    })
+
+    it('should posts a text in a merchants webpage', async () => {
+
+        const response = await request(app)
+            .post('/api/paginaComercio/textos/' + idWeb)
+            .set('Accept', 'application/json')
+            .auth(tokenComercio, { type: 'bearer' })
+            .send({
+
+                "textos": [
+                    
+                    "El Media Markt de Madrid es el mejor de todos",
+                    "Siempre tienen los mejores precios y productos"
+                    
+                ]
+
+            })
+            expect(response.body.acknowledged).toEqual(true)
+
+    })
+
+    /*it('should posts a foto in a merchants webpage', async () => {
+
+        const response = await request(app)
+            .post('/api/paginaComercio/fotos/' + idWeb)
+            .set('Accept', 'application/json')
+            .auth(tokenComercio, { type: 'bearer' })
+            .contentDisposition('form-data', 'name="image"', 'filename="otra.png"')
+            .contentType('image/png')
+            .expect(200)
+            expect(response.body.acknowledged).toEqual(true)
+
+    })*/
+
+    it('should delete a merchants webpage', async () => {
+        const response = await request(app)
+            .delete('/api/admin/deleteComercio/' + idWeb)
+            .set('Accept', 'application/json')
+            .auth(tokenAdmin, { type: 'bearer' })
+            .expect(200)
+        expect(response.body.acknowledged).toEqual(true)
+    })
+
+    it('should delete a merchant', async () => {
+        const response = await request(app)
+            .delete('/api/admin/deleteComercio/' + idComercio)
+            .set('Accept', 'application/json')
+            .auth(tokenAdmin, { type: 'bearer' })
+            .expect(200)
+        expect(response.body.acknowledged).toEqual(true)
     })
 })
